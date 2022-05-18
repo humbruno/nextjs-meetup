7c3ee8d2e74c27af039d4695ded5c16f08fd2e88
Hum Bruno <hsbruno1@gmail.com>
2022-05-18 13:59:14 +0100

    feat: layout component and page navigation

---
+++ pages/[meetupId]/index.jsx
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG/2560px-Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG"
+      title="A first meetup"
+      address="Some Street 5, Some City"
+      description="The meetup description"
+    />
+  );
+};
+
+export default MeetupDetails;
